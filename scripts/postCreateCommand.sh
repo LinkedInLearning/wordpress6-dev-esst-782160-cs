6ef7023fed07eae43bdbfa01826d1a5268be2337
Thomas Rose <mail@tom-rose.de>
2022-12-31 20:51:20 +0000

    auto install

--- scripts/postCreateCommand.sh
+++ scripts/postCreateCommand.sh
@@ -8,3 +8,13 @@ echo xdebug.log_level=0 | sudo tee -a /usr/local/etc/php/conf.d/xdebug.ini
 
 echo "Starting docker"
 docker-compose up -d
+
+file="../htdocs/wp-config.php"
+while test ! -f "$file"
+do
+  sleep 1
+done
+
+chmod 777 htdocs/ -R 
+make update-wp-config
+make install
